578b48c8f301129fe2170655d361018384acda49
Pranaykunisetty590 <pranaykunisetty785@gmail.com>
2026-03-18 11:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit as st
 # Must be first Streamlit command
 st.set_page_config(page_title="Crime Prediction System", page_icon="🚔", layout="wide")
 
-import matplotlib
+import matplotlib.pyplot as plt
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
